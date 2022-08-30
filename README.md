63dd3fb10ba3f09431494a84668535d57a33feae
Gianluca Astorino <gianlucaastorino@Gianlucas-MacBook-Pro-2.local>
2022-08-30 14:05:25 -0400

    pushed all data

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# mongo-winkle
+To install this package, run:
+
+```npm install mongo-winkle```
+
+Then, view examples below about how to use this:
+
+```let mongo_winkle = require('mongo-winkle')```
+
+```let results = await mongo_winkle(username, password, mongo_uri, cluster, collection)```
+```if (results == true) return true```
+
+you need to pass 5 arguments to the api.
+
+The ```username``` and ```password``` of the person trying to login.
+
+The ```mongo_uri```, ```cluster```, and ```collection``` of your mongo ```cluster```.
+
+In your mongo collection, we are looking for ```username``` to compare to the username you submit to the api.
+
+If you want to change this, go into the ```node module``` and find ```'username': username``` and change it to whatever you want!
+
+Then you're good to go. This 1 liner will help you easily create a login system in 1 line of code.
+
+Note: 
+    It is up to you to make this more secure. We made is secure using bcrypt hashing but it may not be up to your standards.
+    We take no responsibility for anything resulting in this package failing
---
+++ index.js
@@ -0,0 +1,46 @@
+let {MongoClient} = require("mongodb")
+let uri = ''
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+
+
+module.exports = async function(username, password, mongo_uri, cluster, collection) {
+    
+    let client = new MongoClient(mongo_uri)
+    let database = client.db(cluster)
+    let collection_name = database.collection(collection)
+
+    if (!username || !password || !username && !password) {
+        reject(false)
+    } else {
+    return new Promise((resolve, reject)=>{
+        MongoClient.connect(mongo_uri, async function(err, db) {
+                if (err) {
+                    console.log(err);
+                    reject(false)
+                }
+                
+                collection_name.find({'username': username}).toArray(async function(e,doc){
+                    if(doc.length === 0) {
+                        resolve(false)
+                        db.close();
+                    } else {
+
+                    let hashed_pw = ""
+                    for (var i = 0; i < doc.length; i++) {
+                        hashed_pw = doc[i].password
+                    }
+
+                    bcrypt.compare(password, hashed_pw, async function(err, result) {
+                        if (result == true) {
+                            resolve(true)
+                        } else {
+                            resolve(false)
+                        }
+                });
+            }
+            })
+            })
+        })
+    }
+ } 
